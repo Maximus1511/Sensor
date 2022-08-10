b4b90cae563d2602335b43c31d16b02748f6321a
Maksim Kryzhanovskii <krimaxim2010@yandex.ru>
2022-08-11 02:08:12 +0300

    Prepate to create client
    new project created, dependencies added

---
+++ SensorClient/src/main/java/ru/kryz/max/App.java
@@ -0,0 +1,14 @@
+package ru.kryz.max;
+
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        RestTemplate restTemplate =  new RestTemplate();
+    }
+}
