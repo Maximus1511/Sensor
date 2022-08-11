c073443698ffc8fb954ec5165885addf9af172f4
Maksim Kryzhanovskii <krimaxim2010@yandex.ru>
2022-08-11 03:10:14 +0300

    The simplest sensor client created
    you can add single measurement

--- SensorClient/src/main/java/ru/kryz/max/App.java
+++ SensorClient/src/main/java/ru/kryz/max/App.java
@@ -1,14 +1,69 @@
 package ru.kryz.max;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * Hello world!
- *
- */
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args ) {
-        RestTemplate restTemplate =  new RestTemplate();
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("Input sensor name:");
+        String sensorName =  scanner.nextLine();
+        //register new sensor
+        registerSensor(sensorName);
+
+        //send measurement
+        sendMeasurement(20.22, false, sensorName);
+
+        scanner.close();
+    }
+
+    private static void registerSensor(String sensorName){
+        final String url = "http://localhost:8080/sensors/registration";
+
+        Map<String, Object> jsonData =  new HashMap<>();
+        jsonData.put("name", sensorName);
+
+        doPostRequestWithJSON(url, jsonData);
+    }
+
+    private static void sendMeasurement(double value, boolean raining, String sensorName){
+        final String url = "http://localhost:8080/meas/add";
+
+        Map<String, Object> jsonData = new HashMap<>();
+        jsonData.put("value", value);
+        jsonData.put("raining", raining);
+        jsonData.put("sensor", Map.of("name", sensorName));
+
+        doPostRequestWithJSON(url, jsonData);
+    }
+
+    private static void doPostRequestWithJSON(String url, Map<String, Object> jsonData) {
+        final RestTemplate restTemplate =  new RestTemplate();
+        final HttpHeaders headers = new HttpHeaders();
+
+        //determine type
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        //Wrap
+        HttpEntity<Object> request = new HttpEntity<>(jsonData, headers);
+
+        //send
+        try {
+            restTemplate.postForObject(url, request, String.class);
+            System.out.println("Request was successfully sent to the server");
+        } catch (HttpClientErrorException e) {
+            System.out.println("Error");
+            System.out.println(e.getMessage());
+        }
     }
 }
